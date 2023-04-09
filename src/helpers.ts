c4d606de6875941a85b3d9cd4179edb05bd73d6b
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-04-09 10:09:00 +0200

    Improve breakpoints

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,13 +1,13 @@
 import { breakpoints } from './tokens';
 
-export const minSm = `@media (min-width: ${breakpoints.sm})`;
-export const minMd = `@media (min-width: ${breakpoints.md})`;
-export const minLg = `@media (min-width: ${breakpoints.lg})`;
-export const minXl = `@media (min-width: ${breakpoints.xl})`;
-export const min2xl = `@media (min-width: ${breakpoints['2xl']})`;
+export const minSm = `@media (min-width: ${breakpoints.sm}px)`;
+export const minMd = `@media (min-width: ${breakpoints.md}px)`;
+export const minLg = `@media (min-width: ${breakpoints.lg}px)`;
+export const minXl = `@media (min-width: ${breakpoints.xl}px)`;
+export const min2xl = `@media (min-width: ${breakpoints['2xl']}px)`;
 
-export const maxSm = `@media (max-width: ${breakpoints.sm})`;
-export const maxMd = `@media (max-width: ${breakpoints.md})`;
-export const maxLg = `@media (max-width: ${breakpoints.lg})`;
-export const maxXl = `@media (max-width: ${breakpoints.xl})`;
-export const max2xl = `@media (max-width: ${breakpoints['2xl']})`;
+export const maxSm = `@media (max-width: ${breakpoints.sm - 1}px)`;
+export const maxMd = `@media (max-width: ${breakpoints.md - 1}px)`;
+export const maxLg = `@media (max-width: ${breakpoints.lg - 1}px)`;
+export const maxXl = `@media (max-width: ${breakpoints.xl - 1}px)`;
+export const max2xl = `@media (max-width: ${breakpoints['2xl'] - 1}px)`;
--- src/tokens.ts
+++ src/tokens.ts
@@ -89,9 +89,9 @@ export const lineHeights = {
 };
 
 export const breakpoints = {
-  sm: '640px',
-  md: '768px',
-  lg: '1024px',
-  xl: '1280px',
-  '2xl': '1536px',
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  '2xl': 1536,
 };
