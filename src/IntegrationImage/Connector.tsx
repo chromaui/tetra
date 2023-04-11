6180083d33620f293ed4f31c0dbae8bb10987179
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-04-11 16:32:00 +0200

    Add integration image new component

---
+++ src/IntegrationImage/Connector.tsx
@@ -0,0 +1,67 @@
+import React, { FC } from 'react';
+import { styled } from '@storybook/theming';
+import { minMd } from '../_helpers';
+import { color } from '../_tokens';
+
+const ArrowSVG = styled.svg`
+  position: relative;
+  margin-left: -6px;
+  margin-right: 3px;
+  z-index: 1;
+`;
+
+const Arrow = () => (
+  <ArrowSVG
+    xmlns="http://www.w3.org/2000/svg"
+    width="17"
+    height="8"
+    fill="none"
+    viewBox="0 0 17 8"
+  >
+    <g clipPath="url(#clip0_7043_99629)">
+      <path
+        fill={color.pink500}
+        d="M16.354 4.354a.5.5 0 000-.708L13.172.464a.5.5 0 10-.708.708L15.293 4l-2.829 2.828a.5.5 0 10.708.708l3.182-3.182zM0 4.5h16v-1H0v1z"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_7043_99629">
+        <path fill="#fff" d="M0 0H17V8H0z" />
+      </clipPath>
+    </defs>
+  </ArrowSVG>
+);
+
+const Container = styled.div`
+  position: relative;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  margin-left: -3px;
+
+  ${minMd} {
+    margin-left: -5px;
+  }
+`;
+
+const Circle = styled.div`
+  width: 4px;
+  height: 4px;
+  border: 2px solid #fff;
+  background: #ff4785;
+  border-radius: 100%;
+
+  ${minMd} {
+    width: 6px;
+    height: 6px;
+  }
+`;
+
+export const Connector: FC = () => {
+  return (
+    <Container>
+      <Circle />
+      <Arrow />
+    </Container>
+  );
+};
---
+++ src/IntegrationImage/IntegrationImage.tsx
@@ -0,0 +1,62 @@
+import React, { FC, ReactNode } from 'react';
+import { styled } from '@storybook/theming';
+import { color, spacing } from '../_tokens';
+import { Icon } from '../Icon';
+import { Connector } from './Connector';
+import { minMd } from '../_helpers';
+
+interface IntegrationImageProps {
+  children: ReactNode;
+  backgroundColor?: string;
+  withConnector?: boolean;
+}
+
+const Container = styled.figure`
+  display: flex;
+  align-items: center;
+  margin: 0;
+`;
+
+const LogoContainer = styled.div<{ bg?: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+  width: ${spacing[10]};
+  height: ${spacing[10]};
+  border-radius: 8px;
+  background-color: ${({ bg }) => bg || color.gray100};
+
+  > * {
+    transform: scale(0.7);
+  }
+
+  ${minMd} {
+    width: ${spacing[16]};
+    height: ${spacing[16]};
+
+    > * {
+      transform: scale(1);
+    }
+  }
+`;
+
+export const IntegrationImage: FC<IntegrationImageProps> = ({
+  backgroundColor,
+  children,
+  withConnector,
+}) => {
+  return (
+    <Container>
+      {withConnector && (
+        <>
+          <LogoContainer bg="#ff4785">
+            <Icon name="storybook" size={44} color="white" />
+          </LogoContainer>
+          <Connector />
+        </>
+      )}
+      <LogoContainer bg={backgroundColor}>{children}</LogoContainer>
+    </Container>
+  );
+};
