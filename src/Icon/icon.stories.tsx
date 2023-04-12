e2bece660af2dfe8ab603f6e82212943054178fa
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-04-12 09:32:35 +0200

    Fix Icon name

--- src/Icon/icon.stories.tsx
+++ src/Icon/icon.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Icon } from './index';
+import { Icon } from './Icon';
 
 const meta = {
   title: 'Icon',
