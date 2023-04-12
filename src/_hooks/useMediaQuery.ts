f258e08752dd7048c847fb0e0647c38e79e50b7b
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-04-12 09:50:59 +0200

    Update useMediaQuery.ts

--- src/_hooks/useMediaQuery.ts
+++ src/_hooks/useMediaQuery.ts
@@ -2,7 +2,19 @@ import { useEffect, useState } from 'react';
 import { breakpoint } from '../_tokens';
 
 export function useMediaQuery(
-  query: 'minBase' | 'minSm' | 'minMd' | 'minLg' | 'minXl' | 'min2Xl'
+  query:
+    | 'minBase'
+    | 'minSm'
+    | 'minMd'
+    | 'minLg'
+    | 'minXl'
+    | 'min2Xl'
+    | 'maxBase'
+    | 'maxSm'
+    | 'maxMd'
+    | 'maxLg'
+    | 'maxXl'
+    | 'max2Xl'
 ): boolean {
   let newQuery = '';
   if (query === 'minBase') newQuery = `(min-width: ${breakpoint.base}px)`;
@@ -11,6 +23,11 @@ export function useMediaQuery(
   if (query === 'minLg') newQuery = `(min-width: ${breakpoint.lg}px)`;
   if (query === 'minXl') newQuery = `(min-width: ${breakpoint.xl}px)`;
   if (query === 'min2Xl') newQuery = `(min-width: ${breakpoint['2xl']}px)`;
+  if (query === 'maxSm') newQuery = `(max-width: ${breakpoint.sm - 1}px)`;
+  if (query === 'maxMd') newQuery = `(max-width: ${breakpoint.md - 1}px)`;
+  if (query === 'maxLg') newQuery = `(max-width: ${breakpoint.lg - 1}px)`;
+  if (query === 'maxXl') newQuery = `(max-width: ${breakpoint.xl - 1}px)`;
+  if (query === 'max2Xl') newQuery = `(max-width: ${breakpoint['2xl'] - 1}px)`;
 
   const getMatches = (query: string): boolean => {
     // Prevents SSR issues
