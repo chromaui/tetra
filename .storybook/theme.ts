2d801cff7efc82510f4e46155372111ae4cf8205
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-04-21 10:20:08 +0100

    Add logo

---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Tetra',
+  brandUrl: 'https://tetra.chromatic.com',
+  brandImage: 'src/logo.png',
+  brandTarget: '_self',
+});
