3b8065c62cc8cf21f37fb0836bcc4ff69207d7f1
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-04-26 12:42:44 +0100

    Update Popover.stories.tsx

--- src/Popover/Popover.stories.tsx
+++ src/Popover/Popover.stories.tsx
@@ -2,7 +2,7 @@ import React, { Meta, StoryObj } from '@storybook/react';
 
 import { Popover } from './Popover';
 import { Button } from '../Button';
-import { Text } from '../Text';
+import { styled } from '@storybook/theming';
 
 const meta: Meta<typeof Popover> = {
   title: 'Overlay/Popover',
@@ -13,13 +13,39 @@ const meta: Meta<typeof Popover> = {
 export default meta;
 type Story = StoryObj<typeof Popover>;
 
+const Container = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
 export const Base: Story = {
-  args: {
-    children: 'Hello World',
-  },
-  render: ({ children }) => (
+  render: () => (
     <Popover trigger={<Button>I'm a button</Button>}>
-      <Text lineHeightAuto>{children}</Text>
+      <Container>
+        <Left>
+          <Button size="sm">Hello</Button>
+          <Button size="sm">Hello</Button>
+          <Button size="sm">Hello</Button>
+        </Left>
+        <Right>
+          <Button size="sm">Hello</Button>
+          <Button size="sm">Hello</Button>
+          <Button size="sm">Hello</Button>
+        </Right>
+      </Container>
     </Popover>
   ),
   parameters: {
