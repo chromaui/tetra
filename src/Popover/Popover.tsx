c54ecc0fcf8d98e936b4c79300ba10e27a25d884
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-04-26 12:36:54 +0100

    Update Popover.tsx

--- src/Popover/Popover.tsx
+++ src/Popover/Popover.tsx
@@ -16,11 +16,8 @@ const PopoverContent = styled(PopoverPrimitive.Content)`
   padding: 20px;
   width: 260px;
   background-color: white;
-  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px,
-    hsl(206 22% 7% / 20%) 0px 10px 20px -15px;
-  animation-duration: 400ms;
-  animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
-  will-change: transform, opacity;
+  box-shadow: hsl(206 22% 7% / 35%) 0px 10px 38px -10px;
+  outline: none;
 `;
 
 const PopoverArrow = styled(PopoverPrimitive.Arrow)`
