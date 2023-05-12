7eeb801e187c9d1a4c00f933cd83619cf6a67f66
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-05-12 17:05:42 +0100

    Add story for NavMobileSection

---
+++ src/Header/NavMobileSection.stories.tsx
@@ -0,0 +1,174 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { NavMobileSection } from './NavMobileSection';
+import React from 'react';
+import { FigmaIcon } from './icons/figma';
+
+const meta: Meta<typeof NavMobileSection> = {
+  title: 'Components/Header/NavMobileSection',
+  component: NavMobileSection,
+};
+
+export default meta;
+type Story = StoryObj<typeof NavMobileSection>;
+
+export const NotCollapsible: Story = {
+  args: {
+    section: {
+      name: 'Features',
+      content: [
+        {
+          title: 'UI Tests',
+          icon: 'contrast',
+          iconColor: 'teal500',
+          href: '/features/ui-tests',
+        },
+        {
+          title: 'UI Review',
+          icon: 'batchaccept',
+          iconColor: 'emerald500',
+          href: '/',
+        },
+        {
+          title: 'Publish Storybook',
+          icon: 'globe',
+          iconColor: 'blue500',
+          href: '/',
+        },
+        {
+          title: 'Visual Test',
+          icon: 'eye',
+          iconColor: 'purple500',
+          href: '/',
+        },
+        {
+          title: 'Interaction Test',
+          icon: 'pointerhand',
+          iconColor: 'orange500',
+          href: '/',
+        },
+        {
+          title: 'TurboSnap',
+          icon: 'dashboard',
+          iconColor: 'blue500',
+          href: '/',
+        },
+        {
+          title: 'Figma Plugin',
+          customIcon: <FigmaIcon />,
+          href: '/',
+        },
+      ],
+    },
+    isLast: false,
+  },
+  decorators: [(storyFn) => <>{storyFn()}</>],
+};
+
+export const Max3items: Story = {
+  args: {
+    ...NotCollapsible.args,
+    section: {
+      name: 'Features',
+      maxItems: 3,
+      content: [
+        {
+          title: 'UI Tests',
+          icon: 'contrast',
+          iconColor: 'teal500',
+          href: '/features/ui-tests',
+        },
+        {
+          title: 'UI Review',
+          icon: 'batchaccept',
+          iconColor: 'emerald500',
+          href: '/',
+        },
+        {
+          title: 'Publish Storybook',
+          icon: 'globe',
+          iconColor: 'blue500',
+          href: '/',
+        },
+        {
+          title: 'Visual Test',
+          icon: 'eye',
+          iconColor: 'purple500',
+          href: '/',
+        },
+        {
+          title: 'Interaction Test',
+          icon: 'pointerhand',
+          iconColor: 'orange500',
+          href: '/',
+        },
+        {
+          title: 'TurboSnap',
+          icon: 'dashboard',
+          iconColor: 'blue500',
+          href: '/',
+        },
+        {
+          title: 'Figma Plugin',
+          customIcon: <FigmaIcon />,
+          href: '/',
+        },
+      ],
+    },
+    isLast: false,
+  },
+  decorators: [(storyFn) => <>{storyFn()}</>],
+};
+
+export const Collapsible: Story = {
+  args: {
+    section: {
+      name: 'Features',
+      collapsible: true,
+      content: [
+        {
+          title: 'UI Tests',
+          icon: 'contrast',
+          iconColor: 'teal500',
+          href: '/features/ui-tests',
+        },
+        {
+          title: 'UI Review',
+          icon: 'batchaccept',
+          iconColor: 'emerald500',
+          href: '/',
+        },
+        {
+          title: 'Publish Storybook',
+          icon: 'globe',
+          iconColor: 'blue500',
+          href: '/',
+        },
+        {
+          title: 'Visual Test',
+          icon: 'eye',
+          iconColor: 'purple500',
+          href: '/',
+        },
+        {
+          title: 'Interaction Test',
+          icon: 'pointerhand',
+          iconColor: 'orange500',
+          href: '/',
+        },
+        {
+          title: 'TurboSnap',
+          icon: 'dashboard',
+          iconColor: 'blue500',
+          href: '/',
+        },
+        {
+          title: 'Figma Plugin',
+          customIcon: <FigmaIcon />,
+          href: '/',
+        },
+      ],
+    },
+    isLast: false,
+  },
+  decorators: [(storyFn) => <>{storyFn()}</>],
+};
