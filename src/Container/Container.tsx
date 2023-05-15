fdc0fa5ea5167eafc067700c4969cdf8d5b0a2db
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-05-16 00:39:43 +0100

    Bring back breakpoint tokens + container margins

--- src/Container/Container.tsx
+++ src/Container/Container.tsx
@@ -1,28 +1,36 @@
-import { ReactNode } from 'react';
 import { styled } from '@storybook/theming';
 import {
   BlockWithOptionsForContainer,
-  BlockWithOptionsForContainerProps,
+  min2xl,
   minSm,
+  minXl,
 } from '../_helpers';
 import { spacing } from '../_tokens';
 
-interface ContainerProps extends BlockWithOptionsForContainerProps {
-  children: ReactNode;
-  maxWidth?: number;
-}
+export const pageMargin = 5.55555;
 
-export const Container = styled(BlockWithOptionsForContainer)<{
-  maxWidth?: ContainerProps['maxWidth'];
-}>`
-  max-width: ${({ maxWidth }) => maxWidth || 1248}px;
-  margin-left: auto;
-  margin-right: auto;
+export const Container = styled(BlockWithOptionsForContainer)`
   padding-left: ${spacing[5]};
   padding-right: ${spacing[5]};
 
   ${minSm} {
-    padding-left: ${spacing[10]};
-    padding-right: ${spacing[10]};
+    margin-left: ${pageMargin}%;
+    margin-right: ${pageMargin}%;
+  }
+
+  ${minXl} {
+    margin-left: ${pageMargin * 2}%;
+    margin-right: ${pageMargin * 2}%;
+  }
+
+  ${min2xl} {
+    margin-left: ${pageMargin * 3}%;
+    margin-right: ${pageMargin * 3}%;
+  }
+
+  // To review if this is needed
+  @media (min-width: 2400px) {
+    margin-left: ${pageMargin * 4}%;
+    margin-right: ${pageMargin * 4}%;
   }
 `;
