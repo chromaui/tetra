85d19f3548385a98fd6c9d80ca3692927ada45b7
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-05-16 10:34:08 +0100

    Update NavDesktop.tsx

--- src/Header/NavDesktop.tsx
+++ src/Header/NavDesktop.tsx
@@ -1,48 +1,5 @@
-import { styled } from '@storybook/theming';
 import React, { FC } from 'react';
-import * as NavigationMenu from '@radix-ui/react-navigation-menu';
-import { useHeaderContext } from './context';
-// import { NavDesktopLink } from './NavDesktopLink';
-// import { NavDesktopTrigger } from './NavDesktopTrigger';
-
-const NavigationMenuRoot = styled(NavigationMenu.Root)`
-  position: relative;
-  display: flex;
-  justify-content: center;
-  z-index: 100;
-`;
-
-// const NavigationMenuList = styled(NavigationMenu.List)`
-//   display: flex;
-//   justify-content: center;
-//   list-style: none;
-//   margin: 0;
-//   padding: 0;
-//   gap: 9px;
-// `;
-
-// const NavigationMenuItem = styled(NavigationMenu.Item)`
-//   position: relative;
-// `;
 
 export const NavDesktop: FC = () => {
-  const { desktopData } = useHeaderContext();
-
-  return (
-    <NavigationMenuRoot>
-      Bonjour
-      {/* <NavigationMenuList>
-        {desktopData &&
-          desktopData.map((item) => (
-            <NavigationMenuItem key={item.name}>
-              {item.menu ? (
-                <NavDesktopTrigger key={item.name} item={item} />
-              ) : (
-                <NavDesktopLink key={item.name} item={item} />
-              )}
-            </NavigationMenuItem>
-          ))}
-      </NavigationMenuList> */}
-    </NavigationMenuRoot>
-  );
+  return <div>Bonjour</div>;
 };
