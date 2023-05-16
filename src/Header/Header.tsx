36f09fd94f3bf1e3e2be9ae038aaca10f9558b23
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-05-16 10:17:38 +0100

    Update Header.tsx

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,15 +1,11 @@
 import React, { FC } from 'react';
 import { styled } from '@storybook/theming';
 import { HeaderProvider, useHeaderContext } from './context';
-import { Logo } from '../Logo';
 import { spacing } from '../_tokens';
 import { Container } from '../Container';
 import { NavDesktop } from './NavDesktop';
-import { NavMobile } from './NavMobile';
-import { useMediaQuery } from '../_hooks/useMediaQuery';
 import { HeaderProps } from './types';
 import { minSm } from '../_helpers';
-import { LinkWithWrapper } from '../LinkWithWrapper';
 
 interface WrapperProps {
   desktopBreakpoint?: HeaderProps['desktopBreakpoint'];
@@ -34,29 +30,6 @@ const Left = styled.div`
     align-items: center;
     gap: ${spacing[6]};
   }
-`;
-
-const LogoLink = styled(LinkWithWrapper)`
-  display: block;
-  padding: ${spacing[2]};
-  font-size: 0;
-  border-radius: 6px;
-  margin-left: -${spacing[2]};
-
-  &:focus-visible {
-    box-shadow: 0 0 0 2px rgba(30, 167, 253, 0.3);
-    outline: none;
-  }
-
-  ${minSm} {
-    margin-top: -4px;
-  }
-`;
-
-const Right = styled.div`
-  display: flex;
-  gap: ${spacing[6]};
-  align-items: center;
 `;
 
 export const Header: FC<HeaderProps> = ({
@@ -94,32 +67,14 @@ export const Header: FC<HeaderProps> = ({
 };
 
 const HeaderWithProvider: FC = () => {
-  const {
-    theme,
-    desktopBreakpoint,
-    logo,
-    logoHeightDesktop,
-    logoHeightMobile,
-    logoHref,
-    desktopRight,
-  } = useHeaderContext();
-  const isDesktop = useMediaQuery({ min: desktopBreakpoint || 1024 });
+  const { desktopBreakpoint } = useHeaderContext();
 
   return (
     <Container>
       <Wrapper desktopBreakpoint={desktopBreakpoint}>
-        {/* <Left>
-          <LogoLink href={logoHref || '/'} aria-label="Home">
-            <Logo
-              name={logo || 'chromatic'}
-              height={isDesktop ? logoHeightDesktop : logoHeightMobile}
-              theme={theme}
-            />
-          </LogoLink>
-          {isDesktop && <NavDesktop />}
-        </Left> */}
-        {isDesktop && <Right>{desktopRight}</Right>}
-        {/* {!isDesktop && <NavMobile />} */}
+        <Left>
+          <NavDesktop />
+        </Left>
       </Wrapper>
     </Container>
   );
