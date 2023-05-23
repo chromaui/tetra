b008332cea0794358cd92ebac84598ece000a723
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-05-23 10:34:36 +0100

    Update Storybook

--- src/Icon/icon.stories.tsx
+++ src/Icon/icon.stories.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Icon } from './Icon';
 
-const meta = {
+const meta: Meta<typeof Icon> = {
   title: 'Components/Icon',
   component: Icon,
-} satisfies Meta<typeof Icon>;
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const IconStory: Story = {
-  name: 'Icon Component',
-  render: (args) => <Icon name={args.name} size={args.size} />,
+export const IconComponent: Story = {
   args: {
     size: 14,
     name: 'plus',
