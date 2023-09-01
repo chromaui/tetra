eee297baf8d883d24d1450894305bafe82fca864
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-09-01 09:35:12 +0100

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -15,11 +15,11 @@ export default defineConfig({
   // There is a bug in Radix - https://github.com/radix-ui/primitives/issues/1848
   // Remove this one when it's fixed
   noExternal: [
-    '@storybook/theming',
     '@radix-ui/react-navigation-menu',
     '@radix-ui/react-popover',
     '@radix-ui/react-collapsible',
     'framer-motion',
-    '@emotion/*',
+    // '@storybook/theming',
+    // '@emotion/*',
   ],
 });
